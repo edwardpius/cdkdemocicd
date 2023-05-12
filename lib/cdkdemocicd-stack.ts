c9d8c66a533e4258cfe1c86e72df18f403cfed60
Edward Pius <edward.pius@gmail.com>
2023-05-12 16:34:35 -0700

    Update to include lambda function

--- lib/cdkdemocicd-stack.ts
+++ lib/cdkdemocicd-stack.ts
@@ -1,6 +1,9 @@
 import * as cdk from 'aws-cdk-lib';
 import { Construct } from 'constructs';
 import { CodePipeline, CodePipelineSource, ShellStep } from 'aws-cdk-lib/pipelines';
+import { PipelineAppStage } from './cdkdemocicd-app-stack' ;
+import { ManualApprovalStep } from 'aws-cdk-lib/pipelines';
+import { Code } from 'aws-cdk-lib/aws-lambda' ;
 
 export class CdkdemocicdStack extends cdk.Stack {
   constructor(scope: Construct, id: string, props?: cdk.StackProps) {
@@ -20,5 +23,15 @@ export class CdkdemocicdStack extends cdk.Stack {
         ],
       }),
     });
+
+    const testingStage = democicdpipeline.addStage(new PipelineAppStage(this, 'test', {
+      env: { account: '288417637285', region: 'us-west-2'}
+    }));
+
+    testingStage.addPost(new ManualApprovalStep('approval'));
+
+    const prodStage = democicdpipeline.addStage(new PipelineAppStage(this, 'prod', {
+      env: { account: '288417637285', region: 'us-west-2'}
+    }));
   }
 }
